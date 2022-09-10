4e26b6199fc3556cab626ba01e1991af7bd53f1b
k1105 <kanapon1105@yahoo.co.jp>
2022-09-11 03:08:56 +0900

    レイヤーごとの配色を設定

--- lib/addDataOnMap.ts
+++ lib/addDataOnMap.ts
@@ -2,14 +2,35 @@ import bikeStation from "../public/data/bikeStation";
 import trainStation from "../public/data/trainStation";
 import trainLine from "../public/data/trainLine";
 import meter0 from "../public/data/network/meter0";
+import meter500 from "../public/data/network/meter500";
+import meter1000 from "../public/data/network/meter1000";
+import meter1500 from "../public/data/network/meter1500";
+import meter2000 from "../public/data/network/meter2000";
+import meter2500 from "../public/data/network/meter2500";
+import meter3000 from "../public/data/network/meter3000";
+import meter3500 from "../public/data/network/meter3500";
+import meter4000 from "../public/data/network/meter4000";
+import meter4500 from "../public/data/network/meter4500";
+import meter5000 from "../public/data/network/meter5000";
 
 export default function addDataOnMap(map: mapboxgl.Map) {
   // Add the vector tileset as a source.
   const metersToPixelsAtMaxZoom = (meters: number, latitude: number) =>
     meters / 0.075 / Math.cos((latitude * Math.PI) / 180);
 
-  let pedestrianRadius = 700;
-  let riderRadius = pedestrianRadius * 3;
+  const network = [
+    meter0,
+    meter500,
+    meter1000,
+    meter1500,
+    meter2000,
+    meter2500,
+    meter3000,
+    meter3500,
+    meter4000,
+    meter4500,
+    meter5000,
+  ];
 
   map.addSource("bike-station", {
     type: "geojson",
@@ -35,17 +56,35 @@ export default function addDataOnMap(map: mapboxgl.Map) {
     },
   });
 
-  map.addSource("bike-line", {
-    type: "geojson",
-    data: {
-      type: "Feature",
-      properties: {},
-      geometry: {
-        type: "MultiLineString",
-        coordinates: meter0(),
+  for (let i = network.length - 1; i >= 0; i--) {
+    const id = `bike-line-${i * 500}`;
+    map.addSource(id, {
+      type: "geojson",
+      data: {
+        type: "Feature",
+        properties: {},
+        geometry: {
+          type: "MultiLineString",
+          coordinates: network[i](),
+        },
       },
-    },
-  });
+    });
+
+    map.addLayer({
+      id: id,
+      type: "line",
+      source: id,
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+        visibility: "none",
+      },
+      paint: {
+        "line-color": `rgba(${4},${169 - (i - 1) * 10},${109},1)`,
+        "line-width": 1,
+      },
+    });
+  }
 
   map.addSource("train-line", {
     type: "geojson",
@@ -58,37 +97,6 @@ export default function addDataOnMap(map: mapboxgl.Map) {
       },
     },
   });
-
-  //   map.addLayer({
-  //     id: "bike-station-position",
-  //     type: "circle",
-  //     source: "bike-station",
-  //     paint: {
-  //       // Make circles larger as the user zooms from z12 to z22.
-  //       "circle-radius": {
-  //         base: 3,
-  //         stops: [
-  //           [12, 4],
-  //           [22, 200],
-  //         ],
-  //       },
-  //       // Color circles by ethnicity, using a `match` expression.
-  //       "circle-color": "#ffffff",
-  //     },
-  //   });
-
-  //   map.addLayer({
-  //     id: "rider-coverage",
-  //     type: "circle",
-  //     source: "bike-station",
-  //     paint: {
-  //       // Make circles larger as the user zooms from z12 to z22.
-  //       "circle-radius": 3,
-  //       // Color circles by ethnicity, using a `match` expression.
-  //       "circle-color": "#abcabc",
-  //       "circle-opacity": 0.6,
-  //     },
-  //   });
 
   map.addLayer({
     id: "bike-station",
@@ -104,29 +112,10 @@ export default function addDataOnMap(map: mapboxgl.Map) {
         base: 2,
       },
       // Color circles by ethnicity, using a `match` expression.
-      "circle-color": "#ffffff",
+      "circle-color": `rgba(4,220,109,1)`,
       "circle-opacity": 0.8,
     },
   });
-
-  //   map.addLayer({
-  //     id: "rider-covarage",
-  //     type: "circle",
-  //     source: "bike-station",
-  //     paint: {
-  //       // Make circles larger as the user zooms from z12 to z22.
-  //       "circle-radius": {
-  //         stops: [
-  //           [0, 0],
-  //           [20, 0],
-  //         ],
-  //         base: 2,
-  //       },
-  //       // Color circles by ethnicity, using a `match` expression.
-  //       "circle-color": "#ffffff",
-  //       "circle-opacity": 1,
-  //     },
-  //   });
 
   map.addLayer({
     id: "train-station",
@@ -142,29 +131,10 @@ export default function addDataOnMap(map: mapboxgl.Map) {
         base: 2,
       },
       // Color circles by ethnicity, using a `match` expression.
-      "circle-color": "#f5a7aa",
-      "circle-opacity": 0.8,
+      "circle-color": "#fff",
+      "circle-opacity": 0.6,
     },
   });
-
-  //   map.addLayer({
-  //     id: "pedestrian-covarage",
-  //     type: "circle",
-  //     source: "train-station",
-  //     paint: {
-  //       // Make circles larger as the user zooms from z12 to z22.
-  //       "circle-radius": {
-  //         stops: [
-  //           [0, 0],
-  //           [20, 0],
-  //         ],
-  //         base: 2,
-  //       },
-  //       "circle-opacity": 1,
-  //       // Color circles by ethnicity, using a `match` expression.
-  //       "circle-color": "#000000",
-  //     },
-  //   });
 
   map.addLayer({
     id: "train-line",
@@ -175,52 +145,9 @@ export default function addDataOnMap(map: mapboxgl.Map) {
       "line-cap": "round",
     },
     paint: {
-      "line-color": "#f5a7aa",
+      "line-color": "#fff", //f5a7aa
       "line-width": 3,
-      "line-opacity": 0.8,
+      "line-opacity": 0.3,
     },
   });
-
-  map.addLayer({
-    id: "bike-line",
-    type: "line",
-    source: "bike-line",
-    layout: {
-      "line-join": "round",
-      "line-cap": "round",
-    },
-    paint: {
-      "line-color": "#04A96D",
-      "line-width": 1,
-    },
-  });
-
-  map.moveLayer("bike-line", "country-label");
-
-  //   setInterval(() => {
-  //     //i += 0.001;
-  //     map.setPaintProperty("pedestrian-covarage", "circle-radius", {
-  //       stops: [
-  //         [0, 0],
-  //         [
-  //           20,
-  //           metersToPixelsAtMaxZoom(
-  //             Math.min(pedestrianRadius * i, 5000),
-  //             35.72902
-  //           ),
-  //         ],
-  //       ],
-  //       base: 2,
-  //     });
-  //     map.setPaintProperty("rider-covarage", "circle-radius", {
-  //       stops: [
-  //         [0, 0],
-  //         [
-  //           20,
-  //           metersToPixelsAtMaxZoom(Math.min(riderRadius * i, 15000), 35.72902),
-  //         ],
-  //       ],
-  //       base: 2,
-  //     });
-  //   }, 20);
 }
